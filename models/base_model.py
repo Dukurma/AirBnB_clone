bc7dc939d885b6601ba569595abbedb797685b12
CzarProCoder <juliusczar36@gmail.com>
2023-05-11 15:51:55 +0300

    Created the core directories + files for AirBnb Clone

---
+++ models/base_model.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+Module containing the base class of all our models
+"""
+
+class Base_model:
+    """
+    """
+    def __init__(self, id, created_at, updated_at):
+        """
+        Initializes the base model
+        """
+        pass
+
+    def save():
+        """
+        Saves a newly added instance
+        """
+        pass
+
+    def to_json()
+        """
+        Converts to json
+        """
+        pass
