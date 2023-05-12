b02807afa8ad929cbf1cde02ef0f60d5c606726f
Dukurma <barryyabilsu@gmail.com>
2023-05-12 15:32:15 -0700

    updated the classes and console

--- console.py
+++ console.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 """Create the AirBnB  console."""
 import cmd
+import json
+from shlex import split
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
+cclasses = {"BaseModel", "FileStorage", "User", "State",
+               "City", "Amenity", "Place", "Review"}
 
 class HBNBCommand(cmd.Cmd):
     """Defines the AirBnB command interpreter.
@@ -45,6 +57,96 @@ class HBNBCommand(cmd.Cmd):
         """EOF signal to exit the program."""
         print("")
         return True
+
+    def do_create(self, args):
+        """ Creates a new instance """
+        if not (args):
+            print("** class name missing **")
+        elif args not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        else:
+            instance = eval[args]()
+            instance.save()
+            print(instance.id)
+
+    def do_show(self, args):
+        """ Prints str representation of an instance """
+        if not (args):
+            print("** class name missing **")
+        else:
+            args = args.split()
+            if len(args) != 2:
+                print("** instance id missing **")
+            elif args[0] not in classes:
+                print("** class doesn't exist **")
+            else:
+                for k, v in storage.all().items():
+                    if args[1] == v.id:
+                        print(v)
+                        return
+                print("** no instance found **")
+
+    def do_destroy(self, args):
+        """ Deletes an instance based on the class name and id """
+        args = args.split()
+        if not args:
+            print("** class name missing **")
+            return
+        elif len(args) < 2:
+            print("** instance id missing **")
+            return
+        if args[0] not in classes:
+            print("** class doesn't exist **")
+            return
+        for k, v in storage.all().items():
+            if args[1] == v.id:
+                del storage.all()[k]
+                storage.save()
+                return
+        print("** no instance found **")
+
+    def do_all(self, args):
+        """ Prints all str representation of all instances """
+        split_args = shlex.split(args)
+        n_list = []
+        dict_json = models.storage.all()
+        if args:
+            try:
+                for key in models.storage.all():
+                    if split_args[0] == key.split('.')[0]:
+                        n_list.append(str(dict_json[key]))
+                print(n_list)
+            except Exception:
+                print("** class doesn't exist **")
+        else:
+            for key in models.storage.all():
+                n_list.append(str(models.storage.all()[key]))
+            print(n_list)
+
+    def do_update(self, args):
+        """ Updates an instance based on the class name and id """
+        args = args.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] in classes:
+            if len(args) > 1:
+                key = args[0] + '.' + args[1]
+                if key in storage.all():
+                    if len(args) > 2:
+                        if len(args) > 3:
+                            setattr(storage.all()[key], args[2], args[3])
+                            storage.all()[key].save()
+                        else:
+                            print("** value missing **")
+                    else:
+                        print("** attribute name missing **")
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
 
 
 if __name__ == '__main__':
