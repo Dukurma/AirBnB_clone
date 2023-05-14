47e5b9fd68e904b334e32cd8ac2b3b997ad84824
Dukurma <barryyabilsu@gmail.com>
2023-05-14 14:14:10 -0700

    code review

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,7 +22,7 @@ class FileStorage:
 
     def all(self):
         ''' Returns the dictionary __objects '''
-        return(FileStorage.__objects)
+        return (FileStorage.__objects)
 
     def new(self, obj):
         '''
